16ad62d5f39a349a3a6beb26af1b4e0373c8bf0b
Jean Leon <jean_leo2000@hotmail.com>
2020-03-01 15:47:46 -0500

    Hot Module Replacement

--- webpack-dev-server/webpack.config.js
+++ webpack-dev-server/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 module.exports = {
   entry: {
     home: path.resolve(__dirname, "src/js/index.js")
@@ -10,20 +11,23 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "js/[name].js"
   },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: MiniCSSExtractPlugin.loader }, "css-loader"]
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      title: "Plugins"
-    }),
-    new MiniCSSExtractPlugin({
-      filename: "css/[name].css"
+      title: "Hot Module Replacement"
     })
   ]
 };
